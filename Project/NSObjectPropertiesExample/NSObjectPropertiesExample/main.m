d88dabc30914057f181003a6e880576a4ac81fb8
Joseph Constan <jcon5294@gmail.com>
2013-12-17 23:52:38 -0500

    added two methods (and two conveniences for them) to NSO+P

---
+++ Project/NSObjectPropertiesExample/NSObjectPropertiesExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSObjectPropertiesExample
+//
+//  Created by Joseph Constantakis on 12/17/13.
+//  Copyright (c) 2013 Joseph Constan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
